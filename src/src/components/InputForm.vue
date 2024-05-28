6bac1bdf4d0305e5fc9ac671e3bb663e5ac78da1
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-05-28 21:29:14 +0200

    cleanup

--- src/src/components/InputForm.vue
+++ src/src/components/InputForm.vue
@@ -1,53 +1,59 @@
 <template>
     <div>
-        <form>
+        <form @submit.prevent="handleSubmit">
             <h2>Eingabeformular für die Erfassung einer Ordnungswidrigkeit</h2>
-            <p>(mit * markierte Felder sind Pflichtfelder)</p>
+            <h5>(mit * markierte Felder sind Pflichtfelder)</h5>
+
             <br><p>Formelle Angaben: </p>
-            <label>Aktenzeichen:</label>
-            <input type="text" required v-model="aktenzeichen" placeholder="*">
+
+            <label>* Aktenzeichen:</label>
+            <input type="text" required v-model="aktenzeichen" placeholder="Bitte eingeben">
             
             <br><p>Angaben zur Person:</p>
+
             <label>Anrede:</label>
-            <input type="text" required v-model="anrede" placeholder="*">
+            <input type="text" required v-model="anrede" placeholder="Bitte eingeben">
 
-            <label>Vorname:</label>
-            <input type="text" required v-model="vorname" placeholder="*">
+            <label>* Vorname:</label>
+            <input type="text" required v-model="vorname" placeholder="Bitte eingeben">
 
-            <label>Nachname:</label>
-            <input type="text" required v-model="nachname" placeholder="*">
+            <label>* Nachname:</label>
+            <input type="text" required v-model="nachname" placeholder="Bitte eingeben">
 
             <label>Titel:</label>
-            <input type="text" v-model="titel">
+            <input type="text" v-model="titel" placeholder="(optional)">
 
-            <label>Geburtsdatum:</label>
-            <input type="date" required v-model="geburtsdatum" placeholder="*">
+            <label>* Geburtsdatum:</label>
+            <input type="date" required v-model="geburtsdatum" placeholder="Bitte eingeben">
 
-            <label>PLZ: </label>
-            <input type="text" required v-model="plz" placeholder="*">
+            <label>* PLZ: </label>
+            <input type="text" required v-model="plz" placeholder="Bitte eingeben">
 
-            <label>Wohnort:</label>
-            <input type="text" required v-model="wohnort" placeholder="*">
+            <label>* Wohnort:</label>
+            <input type="text" required v-model="wohnort" placeholder="Bitte eingeben">
 
-            <label>Straße: </label>
-            <input type="text" required v-model="str" placeholder="*">
+            <label>* Straße: </label>
+            <input type="text" required v-model="str" placeholder="Bitte eingeben">
 
-            <label>Hausnummer: </label>
-            <input type="text" required v-model="hausnummer" placeholder="*">
+            <label>* Hausnummer: </label>
+            <input type="text" required v-model="hausnummer" placeholder="Bitte eingeben">
 
             <br><p>Angaben zur Versicherung:</p>
-            <label>Versicherungsnummer:</label>
-            <input type="text" required v-model="versicherungsnummer" placeholder="*">
+            <label>* Versicherungsnummer:</label>
+            <input type="text" required v-model="versicherungsnummer" placeholder="Bitte eingeben">
 
-            <label>Name der Krankenversicherung:</label>
-            <input type="text" required v-model="krankenversicherungsname" placeholder="*">
+            <label>* Name der Krankenversicherung:</label>
+            <input type="text" required v-model="krankenversicherungsname" placeholder="Bitte eingeben">
 
-            <label>Vertragsunternehmensnummer:</label>
-            <input type="text" required v-model="vertragsunternehmensnummer" placeholder="*">
+            <label>* Vertragsunternehmensnummer:</label>
+            <input type="text" required v-model="vertragsunternehmensnummer" placeholder="Bitte eingeben">
 
             <br><p>Beschreibung des Vorfalls:</p>
-            <input id="beschreibung" type="text" required v-model="beschreibung" placeholder="Schildern Sie hier den Vorfall genau">
+            <input id="beschreibung" type="text" required v-model="beschreibung" placeholder="Bitte eingeben">
 
+            <div class="submit">
+                <button id="submit_button">Absenden</button>
+            </div>
         </form>
     </div>
 </template>
@@ -59,6 +65,12 @@ export default {
         return {
             aktenzeichen: '',
             anrede: '',
+        }
+    },
+    methods: {
+        handleSubmit() {
+            // validate password
+            
         }
     }
 }
@@ -76,15 +88,14 @@ form {
 label {
     color: #3A788F;
     display: inline-block;
-    margin: 15px 5px;
-    font-size: 0.6em;
-    text-transform: uppercase;
-    letter-spacing: 1px;
+    margin: 20px 5px;
+    font-size: 0.7em;
+    letter-spacing: 0.5px;
     font-weight: bold;
 }
 input {
     display: block;
-    padding: 5px 6px;
+    padding: 10px 6px;
     widows: 100%;
     box-sizing: border-box;
     border: none;
@@ -94,13 +105,23 @@ input {
 h2 {
     color: #3A788F;
 }
-#beschreibung {
-    all: initial;
-    font: inherit;
-    color: inherit;
-    display: inherit;
-    box-sizing: inherit;
-    border: none;
-    border-bottom: 1px solid #ddd;
+
+.submit {
+    text-align: center;
+}
+
+#submit_button {
+    background: #4d93aa;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+#submit_button:hover {
+    background-color: #0056b3;
+}
+#submit_button:active {
+    background-color: #003a80;
 }
 </style>
